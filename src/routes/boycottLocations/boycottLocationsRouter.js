77175630e705e2395675bbb85aaf6ee532f24a9e
Quinn <33407017+quinn-mccallum@users.noreply.github.com>
2018-08-02 20:35:49 -0400

    Firestore first steps  (#3)
    * package lock
    
    * npm installed with fresh package-lock
    
    * added basic CORS middleware, getting data from firestore and sending to client
    
    * added post route

--- src/routes/boycottLocations/boycottLocationsRouter.js
+++ src/routes/boycottLocations/boycottLocationsRouter.js
@@ -1,7 +1,9 @@
 // 1. Create an express router
 const express = require('express'),
       router = express.Router();
+const admin = require("firebase-admin");
 
+const db = admin.firestore();
 // 2. define our coffee routes on this express router
 const boycotts = [
     {
@@ -43,18 +45,53 @@ const boycotts = [
 ]
 
 //GET /boycottLocation
-router.get('/boycottLocation', (req, res) => {
-    res.json(boycotts);
+router.get('/boycottLocation', async (req, res) => {
+    db.collection('boycottLocations').get()
+    .then(snapshot=> {
+      const boycottData = [];
+      snapshot.forEach(doc => {
+        boycottData.push({id: doc.id, data: doc.data()});
+      })
+      res.json(boycottData);
+    })
+    .catch(err=>{
+      console.log(err);
+      res.status(404).send();
+    })
 });
 
 //POST /boycottLocation
 router.post('/boycottLocation', (req, res) => {
+    const { name, address, lat, lng, date, text } = req.body;
+    let data = {
+      name: name,
+      address: address,
+      lat: Number(lat),
+      lng: Number(lng),
+      allBoycotts: [
+        {
+          date: Number(date),
+          text: text,
+        }
+      ]
+    }
+
+    db.collection('boycottLocations').add(data)
+      .then(doc => {
+        res.send('Successfully saved boycott.');
+      })
+      .catch(error => {
+        res.send(error);
+        console.log(error);
+      })
+
+
     //take the new data in req.body, and use it to add a new boycott to our array
-    let newBoycott = req.body;
-    boycotts.push(newBoycott);
-    res.send('success');
+    // let newBoycott = req.body;
+    // boycotts.push(newBoycott);
+    // res.send('success');
 });
 
-// 3. now that we have configured our routes on the router we will export it to be 
+// 3. now that we have configured our routes on the router we will export it to be
     // used in the main file
 module.exports = router;
